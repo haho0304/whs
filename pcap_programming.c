1f73b4a2c5c9faf18f938398a6ad12d7c7e88c5b
haho0304 <89858900+haho0304@users.noreply.github.com>
2023-09-24 20:26:38 +0900

    Update pcap_programming.c

--- pcap_programming.c
+++ pcap_programming.c
@@ -14,7 +14,14 @@ struct ethheader {
 
 /* IP Header */
 struct ipheader {
-  unsigned char      iph_ihl:4; //IP header length
+  unsigned char      iph_ihl:4, //IP header length
+                     iph_ver:4; //IP version
+  unsigned char      iph_tos; //Type of service
+  unsigned short int iph_len; //IP Packet length (data + header)
+  unsigned short int iph_ident; //Identification
+  unsigned short int iph_flag:3, //Fragmentation flags
+                     iph_offset:13; //Flags offset
+  unsigned char      iph_ttl; //Time to Live
   unsigned char      iph_protocol; //Protocol type
   struct  in_addr    iph_sourceip; //Source IP address
   struct  in_addr    iph_destip;   //Destination IP address
@@ -24,6 +31,23 @@ struct ipheader {
 struct tcpheader {
     u_short tcp_sport;               /* source port */
     u_short tcp_dport;               /* destination port */
+    u_int   tcp_seq;                 /* sequence number */
+    u_int   tcp_ack;                 /* acknowledgement number */
+    u_char  tcp_offx2;               /* data offset, rsvd */
+#define TH_OFF(th)      (((th)->tcp_offx2 & 0xf0) >> 4)
+    u_char  tcp_flags;
+#define TH_FIN  0x01
+#define TH_SYN  0x02
+#define TH_RST  0x04
+#define TH_PUSH 0x08
+#define TH_ACK  0x10
+#define TH_URG  0x20
+#define TH_ECE  0x40
+#define TH_CWR  0x80
+#define TH_FLAGS        (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
+    u_short tcp_win;                 /* window */
+    u_short tcp_sum;                 /* checksum */
+    u_short tcp_urp;                 /* urgent pointer */
 };
 
 void got_packet(u_char *args, const struct pcap_pkthdr *header,
@@ -33,7 +57,7 @@ void got_packet(u_char *args, const struct pcap_pkthdr *header,
 
   if (ntohs(eth->ether_type) == 0x0800) { // 0x0800 is IP type
     struct ipheader * ip = (struct ipheader *)
-                           (packet + sizeof(struct ethheader)); 
+(packet + sizeof(struct ethheader)); 
     int ip_header_length = ip->iph_ihl * 4;
     struct tcpheader *tcp = (struct tcpheader *)(packet + sizeof(struct ethheader) + ip_header_length);
     
@@ -42,21 +66,18 @@ void got_packet(u_char *args, const struct pcap_pkthdr *header,
     /* determine protocol */
     switch(ip->iph_protocol) {                                 
         case IPPROTO_TCP:
-            printf("Protocol: TCP\n");
+            printf("Protocol: TCP\n");    
     	    printf("Ether From: %s\n", ether_ntoa((struct ether_addr *)eth->ether_shost));
     	    printf("Ether To: %s\n", ether_ntoa((struct ether_addr *)eth->ether_dhost));
-    	    printf("Ip From: %s\n", inet_ntoa(ip->iph_sourceip));   
-    	    printf("Ip To: %s\n", inet_ntoa(ip->iph_destip));  
-    	    printf("TCP From: %d\n", ntohs(tcp->tcp_sport));
+            printf("Ip From: %s\n", inet_ntoa(ip->iph_sourceip));   
+            printf("Ip To: %s\n", inet_ntoa(ip->iph_destip));  
+            printf("TCP From: %d\n", ntohs(tcp->tcp_sport));
     	    printf("TCP To: %d\n", ntohs(tcp->tcp_dport));
             break;
         default:
             printf("   Protocol: others\n");
             break;
     }
-    
-
-    
   }
 }
 
